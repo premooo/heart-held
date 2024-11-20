bcf8caee560a14bf13d0f6f03b9810d40e109ad7
premooo <101117438+premooo@users.noreply.github.com>
2024-11-20 10:13:28 +0800

    initial files

---
+++ client/src/components/PostWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostForm from './PostForm'
+
+const PostWrapper = () => {
+  return (
+    <div className='PostWrapper'>
+      <PostForm />
+    </div>
+  )
+}
+
+export default PostWrapper
