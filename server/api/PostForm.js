02aabf0b3d9f2c94315b6b6ada75d07b1d9d9c3a
premooo <101117438+premooo@users.noreply.github.com>
2024-11-21 14:17:53 +0800

    Update PostForm.js

--- server/api/PostForm.js
+++ server/api/PostForm.js
@@ -35,7 +35,7 @@ const PostForm = () => {
     };
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/post`, {
+      const response = await fetch(`http://localhost:5000/post`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
