9da8a9605beed1b158d631e78da268144e486894
premooo <101117438+premooo@users.noreply.github.com>
2024-11-25 05:15:18 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./db.js');
 const itemModel = require('./models/Post.js');
 const cors = require('cors');
