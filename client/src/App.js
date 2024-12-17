c40aec0577ec30b042af95a716f3592bda3af176
premooo <101117438+premooo@users.noreply.github.com>
2024-12-18 02:23:58 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,26 +15,27 @@ function App() {
         <div className="App">
           <Header />
           <main>
-            <Helmet>
-              <meta charSet="utf-8" />
-              <meta
-                httpEquiv="Content-Security-Policy"
-                content="
-                  default-src 'self'; 
-                  img-src 'self' data: https://fonts.gstatic.com https://assets.codepen.io https://i.scdn.co; 
-                  script-src 'self' https://apis.google.com https://sdk.scdn.co; 
-                  font-src 'self' https://fonts.gstatic.com;
-                  connect-src 'self' http://localhost:5000 https://heart-held-api.onrender.com;
-                  style-src 'self' https://fonts.googleapis.com; 
-                  object-src 'none'; 
-                  frame-src 'self' https://sdk.scdn.com https://open.spotify.com/; 
-                "
-              />
-              <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
-              <meta name="referrer" content="no-referrer" />
-              <title>Heart Held</title>
-              <meta name="description" content="Share heartfelt messages, poems, and stories." />
-            </Helmet>
+           <Helmet>
+            <meta charSet="utf-8" />
+            <meta
+              httpEquiv="Content-Security-Policy"
+              content={`
+                default-src 'self'; 
+                img-src 'self' data: https://fonts.gstatic.com https://assets.codepen.io https://i.scdn.co; 
+                script-src 'self' https://apis.google.com https://sdk.scdn.co; 
+                font-src 'self' https://fonts.gstatic.com;
+                connect-src 'self' http://localhost:5000 https://heart-held-api.onrender.com https://heart-held-api.vercel.app;
+                style-src 'self' https://fonts.googleapis.com; 
+                object-src 'none'; 
+                frame-src 'self' https://sdk.scdn.com https://open.spotify.com/; 
+              `}
+            />
+            <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
+            <meta name="referrer" content="no-referrer" />
+            <title>Heart Held</title>
+            <meta name="description" content="Share heartfelt messages, poems, and stories." />
+          </Helmet>
+
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/submit" element={<PostWrapper />} />
